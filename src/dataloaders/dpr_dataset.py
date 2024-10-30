fb13a906d0bc73f1561f4bb8576bf85751295640
Matin <matinansaripour@gmail.com>
2024-10-30 14:39:14 +0100

    debug

--- src/dataloaders/dpr_dataset.py
+++ src/dataloaders/dpr_dataset.py
@@ -22,15 +22,9 @@ class DPRDataset(Dataset):
 
         positive_idx = row['positive_docs']
         positive_sample = self.doc_embeds[positive_idx]['embeds']
-        positive_sample = torch.tensor(positive_sample, dtype=torch.float32)
 
         negative_indices = row['negative_docs'][1:-1].split(', ')
-        # negative_sample_list = [self.doc_embeds[neg_idx]['embeds'] for neg_idx in negative_indices]
-        negative_sample_list = [self.doc_embeds[neg_idx[1:-1]]['embeds'] for neg_idx in negative_indices]
-        negative_sample = []
-        for sample in negative_sample_list:
-            negative_sample += sample
-        negative_sample = torch.tensor(negative_sample, dtype=torch.float32)
+        negative_sample = [neg for neg_idx in negative_indices for neg in self.doc_embeds[neg_idx[1:-1]]['embeds']]
 
         inputs['positive'] = positive_sample
         inputs['negative'] = negative_sample
