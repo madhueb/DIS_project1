255dab383610b7c22f9c520ba92561ea09e34897
Matin <matinansaripour@gmail.com>
2024-11-04 21:28:13 +0100

    bm25

--- lib/bm25_inf.py
+++ lib/bm25_inf.py
@@ -29,13 +29,15 @@ if __name__ == "__main__":
     parser.add_argument("-dir", "--token_dir", type=Path, default = "./data")
     args = parser.parse_args()
 
-    queries = pd.read_csv(f'{args.token_dir}/test.csv')
+    # queries = pd.read_csv(f'{args.token_dir}/test.csv')
+    queries = pd.read_csv(f'{args.token_dir}/train.csv')
 
     ls = [[] for _ in range(len(queries))]
     queries["docids"] = ls
 
     for lang in LANGS:
-        queries_lang = queries[queries["lang"] == lang][["query"]].reset_index(drop=True)
+        queries_lang = queries[queries["lang"] == lang][["query", "positive_docs"]].reset_index(drop=True)
+        # queries_lang = queries[queries["lang"] == lang][["query"]].reset_index(drop=True)
         tokens = get_tokens(queries_lang["query"].tolist(), lang)
         bm25_ind = bm25s[lang]
         doc_ids = []
@@ -43,8 +45,12 @@ if __name__ == "__main__":
             indices, _ = bm25_ind.match(tokenized_query, k=10)
             doc_ids.append(ids_dict[lang][indices].tolist())
 
-        queries.loc[queries["lang"] == lang, "docids"] = pd.Series(doc_ids, index=queries.loc[queries["lang"] == lang].index)
-
+        # queries.loc[queries["lang"] == lang, "docids"] = pd.Series(doc_ids, index=queries.loc[queries["lang"] == lang].index)
+        acc = 0
+        for i, row in queries_lang.iterrows():
+            if row["positive_docs"] in doc_ids[i]:
+                acc += 1
+        print(f"Accuracy for {lang} : {acc / len(queries_lang)}")
         gc.collect()
 
     queries = queries[["id", "docids"]]
