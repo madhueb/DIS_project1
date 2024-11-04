1e64212300c38bce58d53142e013a10d98f14549
Matin <matinansaripour@gmail.com>
2024-11-04 18:14:41 +0100

    bm25

--- lib/bm25_ind.py
+++ lib/bm25_ind.py
@@ -1,32 +1,21 @@
-from Tf_Idf import Tf_Idf_Vectorizer
-
-import pickle 
-
 import argparse
-from pathlib import Path
+import pickle
 
-
+from lib.bm25.bm25 import BM25
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-dir", "--token_dir", type=Path, default = "./data")
     parser.add_argument("-lang", "--language", type=str, required=True, choices=['ar','de','en','es','fr','it','ko'])
     args = parser.parse_args()
 
-   
-    #Load documents tokenization  : 
     with open(f'{args.token_dir}/tokens_{args.language}.pkl', "rb") as f:
         docs = pickle.load(f)
 
-    
-    tfidf = Tf_Idf_Vectorizer()
-    tfidf.fit_transform(docs)
+    bm25_ind = BM25()
+    bm25_ind.fit(docs)
 
-    #save tfidf model
-    with open(f"tfidf_{args.language}.pkl", "wb") as f:
-        pickle.dump(tfidf, f)
+    with open(f"bm25_{args.language}.pkl", "wb") as f:
+        pickle.dump(bm25_ind, f)
 
-
-    print("Tf-Idf model created for " + args.language)
+    print("BM25 model created for " + args.language)
