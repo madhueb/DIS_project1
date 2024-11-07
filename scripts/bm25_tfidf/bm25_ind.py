06a6a7a3c964a474f024cc5a909c302544590fd7
Matin <matinansaripour@gmail.com>
2024-11-07 13:10:34 +0100

    cleaning

--- scripts/bm25_tfidf/bm25_ind.py
+++ scripts/bm25_tfidf/bm25_ind.py
@@ -13,7 +13,17 @@ if __name__ == "__main__":
     with open(f'{args.token_dir}/tokens_{args.language}.pkl', "rb") as f:
         docs = pickle.load(f)
 
-    bm25_ind = BM25()
+    lang_params = {
+        "ar": {"k1": 1.6, "b": 1.0},
+        "de": {"k1": 2.0, "b": 0.95},
+        "en": {"k1": 1.4, "b": 0.5},
+        "es": {"k1": 1.7, "b": 0.95},
+        "fr": {"k1": 1.8, "b": 0.95},
+        "it": {"k1": 2.0, "b": 0.9},
+        "ko": {"k1": 1.7, "b": 1.0},
+    }
+
+    bm25_ind = BM25(k1=lang_params[args.language]["k1"], b=lang_params[args.language]["b"])
     bm25_ind.fit(docs)
 
     with open(f"bm25_{args.language}.pkl", "wb") as f:
